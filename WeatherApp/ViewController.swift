8dcbc4bf8fd9759509e8dd2cd1333dae92f40392
HirasawaTsuyoshi <tsuyoshihirasawa99@gmail.com>
2016-11-13 12:34:07 +0900

    新規追加

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,19 +7,135 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import SDWebImage
+
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var todayLabel: UILabel!
+    @IBOutlet weak var todayImage: UIImageView!
+    @IBOutlet weak var todayWeatherLabel: UILabel!
+    @IBOutlet weak var todayTemperatureLabel: UILabel!
+    
+    
+    @IBOutlet weak var tomorrowLabel: UILabel!
+    @IBOutlet weak var tomorrowImage: UIImageView!
+    @IBOutlet weak var tomorrowWeatherLabel: UILabel!
+    @IBOutlet weak var tomorrowTemperatureLabel: UILabel!
+    
+    @IBOutlet weak var afterTomorrowLabel: UILabel!
+    @IBOutlet weak var afterTomorrowImage: UIImageView!
+    @IBOutlet weak var afterTomorrowWeatherLabel: UILabel!
+    @IBOutlet weak var afterTomorrowTemperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("http://weather.livedoor.com/forecast/webservice/json/v1?city=030010").responseJSON { (response: DataResponse<Any>) in
+            
+            print(response)
+            
+            if response.result.isFailure == true {
+                self.simpleAlert(title: "通信エラー", message: "通信に失敗しました")
+                return
+            }
+            
+            // "guard let 変数 〜 else" で変数の中身がnilの場合のみの処理が書けます。
+            // ただし最後に必ずreturnで関数を終了させなければいけません。
+            // 変数は以後の関数内でも利用できます。
+            guard let val = response.result.value as? [String: Any] else {
+                self.simpleAlert(title: "通信エラー", message: "通信結果がJSONではありませんでした")
+                return
+            }
+            
+            // responseJSONを使うと辞書形式でも扱えますが、今回はより簡単に扱うためにSwiftyJSONを利用します。
+            let json = JSON(val)
+            
+            // タイトル部分
+            if let title = json["title"].string {
+                self.titleLabel.text = title
+            }
+            
+            // 天気の情報
+            if let forecasts = json["forecasts"].array {
+                
+                // 今日の天気
+                let todayWeather = forecasts[0]
+                
+                self.todayLabel.text = todayWeather["dateLabel"].stringValue
+                
+                if let imgUrl = todayWeather["image"]["url"].string {
+                    self.todayImage.sd_setImage(with: URL(string: imgUrl))
+                }
+                self.todayWeatherLabel.text = todayWeather["telop"].stringValue
+                self.todayTemperatureLabel.text = self.generateTemperatureText(todayWeather["temperature"])
+
+                // 明日の天気
+                let tomorrowWeather = forecasts[1]
+                
+                self.tomorrowLabel.text = tomorrowWeather["dateLabel"].stringValue
+                if let imgUrl = tomorrowWeather["image"]["url"].string {
+                    self.tomorrowImage.sd_setImage(with: URL(string: imgUrl))
+                }
+                self.tomorrowWeatherLabel.text = tomorrowWeather["telop"].stringValue
+                self.tomorrowTemperatureLabel.text = self.generateTemperatureText(tomorrowWeather["temperature"])
+                
+                // 明後日の天気
+                // 気象情報の更新が入るまで明後日の天気情報が存在しない場合があるので要素数をチェックします
+                if forecasts.count >= 3 {
+                    let afterTomorrowWeather = forecasts[2]
+                    
+                    self.afterTomorrowLabel.text = afterTomorrowWeather["dateLabel"].stringValue
+                    if let imgUrl = afterTomorrowWeather["image"]["url"].string {
+                        self.afterTomorrowImage.sd_setImage(with: URL(string: imgUrl))
+                    }
+                    self.afterTomorrowWeatherLabel.text = afterTomorrowWeather["telop"].stringValue
+                    self.afterTomorrowTemperatureLabel.text = self.generateTemperatureText(afterTomorrowWeather["temperature"])
+                }
+            
+            }
+        }
+        
     }
 
+    // 気温のラベル用テキストを生成します。
+    func generateTemperatureText(_ temperature: JSON) -> String {
+        
+        var resultText = ""
+        
+        if let min = temperature["min"]["celsius"].string {
+            resultText += min + "℃"
+        } else {
+            resultText += "-"
+        }
+        
+        resultText += " / "
+        
+        if let max = temperature["max"]["celsius"].string {
+            resultText += max + "℃"
+        } else {
+            resultText += "-"
+        }
+        
+        return resultText
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // 閉じるボタンのみのアラートを表示します。
+    func simpleAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "閉じる", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
